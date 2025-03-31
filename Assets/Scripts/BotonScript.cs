e802e3f3819a686a538c00add2568cda1defb24c
Yuseeeee <49379138@est.ort.edu.ar>
2025-03-31 09:34:09 -0300

    brrr

--- Assets/Scripts/BotonScript.cs
+++ Assets/Scripts/BotonScript.cs
@@ -6,10 +6,7 @@ public class BotonScript : MonoBehaviour
 {
     public GameObject botonGO;
     public CoheteScript coheteScript;
-<<<<<<< HEAD
-=======
 
->>>>>>> 6983be84fb66176146cce47ad8300bd8d5203d68
     // Start is called before the first frame update
     void Start()
     {
